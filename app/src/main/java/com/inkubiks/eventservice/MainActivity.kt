fb1113a9f5f6a4440339cfa67d76eb4ecbb3a962
SavchenkoSofia <sofia.savchenko@sfera.zone>
2023-05-13 13:54:17 +0700

    @frame - [EventsService] - Implemented main bottom navigation

--- app/src/main/java/com/inkubiks/eventservice/MainActivity.kt
+++ app/src/main/java/com/inkubiks/eventservice/MainActivity.kt
@@ -2,10 +2,52 @@ package com.inkubiks.eventservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.navigation.NavigationBarView
+import com.inkubiks.eventservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationBarView.OnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_home -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+
+                R.id.action_staff -> {
+                    navController.navigate(R.id.staffFragment)
+                    true
+                }
+
+                R.id.action_chat -> {
+                    navController.navigate(R.id.chatFragment)
+                    true
+                }
+
+                R.id.action_profile -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        binding?.mainBottomNavigation?.getOrCreateBadge(R.id.action_chat)?.apply {
+            isVisible = true
+            number = 99
+        }
     }
 }
