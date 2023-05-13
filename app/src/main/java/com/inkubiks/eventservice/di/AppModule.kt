d5b1914aefd45d2dbff44f6f76e1066b4f609507
SavchenkoSofia <sofia.savchenko@sfera.zone>
2023-05-13 13:21:27 +0700

    @frame - [EventsService] - Setup modules, navigation and base DI

---
+++ app/src/main/java/com/inkubiks/eventservice/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.inkubiks.eventservice.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+class AppModule {
+    val appModule = module {
+//        viewModel<>()
+    }
+}
